147034e350b794756f497828311a2136977e3642
Lindsay Johnston <lindsaykjohnston@outlook.com>
2022-01-08 12:51:21 -0800

    Changed button to a tag with .linkButton styling. Changed padding to margins on a tags

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends React.Component {
         content2:
           <div>
             <p>My work experiences have varied greatly: from the non-profit sector to the service industry, from education to creative director for a rock-and-roll band. The common threads that weave through all of my professional experiences are creativity and innovation. Whether I am creating a teaching strategy, an app or a t-shirt design, I am always looking to improve upon prior work and present the project in an engaging and professional way. I strive to communicate directly and honestly, work efficiently, and push for female representation in historically male-dominated fields.</p>
-            <button> <a href="https://lkj-website-media-jan2022.s3.us-east-2.amazonaws.com/Lindsay_Johnston_WD_Resume_Aug_2021.pdf" target="_blank" rel="noreferrer">Resume</a> </button>
+             <a className="linkButton" href="https://lkj-website-media-jan2022.s3.us-east-2.amazonaws.com/Lindsay_Johnston_WD_Resume_Aug_2021.pdf" target="_blank" rel="noreferrer">Resume</a> 
           </div>
           
         },
